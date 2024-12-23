a0181c64af2deacaac3a6a2b82a092a3fc2245c7
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-12-23 21:39:01 +0300

    Question 128: O(n) implementation

--- Arrays/128. Longest Consecutive Sequence.py
+++ Arrays/128. Longest Consecutive Sequence.py
@@ -4,43 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        store = {}
-        final = []
-        largest = -1
-        temp = []
-        for i in range(len(nums)):
-            num = nums[i]
-            if largest == -1:
-                temp = [num]
-            elif num - 1 == temp[-1]:
-                temp.append(num)
-                # look for successors in the store
-                next = num + 1
-                while next in store:
-                    temp.append(next)
-                    next += 1
-            else:
-                # look for predecessors in the store
-                temp = [num]
-                prev = num - 1
-                while prev in store:
-                    temp.append(prev)
-                    prev -= 1
-                temp = temp[-1::-1]
+        numSet = set(nums)
+        longest = 0
 
-                # look for successors in the store
-                next = num + 1
-                while next in store:
-                    temp.append(next)
-                    next += 1
-
-            if largest <= len(temp): 
-                largest = len(temp)
-                final = temp
-            store[num] = 1
-
-        return len(final)
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
 
 
 if __name__ == '__main__':
